f30a35d448d9dc72cf2979e2a3086dbb674bd573
pradumna-shelge <82433866+pradumna-shelge@users.noreply.github.com>
2023-04-20 17:10:51 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewpComponent } from './newp/newp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewpComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/newp/newp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newp',
+  templateUrl: './newp.component.html',
+  styleUrls: ['./newp.component.css']
+})
+export class NewpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
